3e7edf4bcc017ce3c55a93fc23caf1703c178079
AlexVRBK <vvvorobok@gmail.com>
2021-05-25 16:37:18 +0300

    PHP.Test

--- index.php
+++ index.php
@@ -1,69 +1,33 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db_name = 'test';
+class Employee
+{
+    public $name;
+    public $age;
+    public $salary;
 
+    // Создаем метод:
+    public function getAge()
+    {
+        return $this->age;
+        {
+            if ($this->getAge()>-18)
+                return 'true';
+            else 'false';
+        }
+        }
 
+}
 
-$link = mysqli_connect($host, $user, $password, $db_name);
+$user1 = new Employee;
+$user1->name = 'Коля';
+$user1->age = 12;
+$user1->salary = 1000;
 
+$user2 = new Employee;
+$user2->name = 'Вася';
+$user2->age = 16;
+$user2->salary = 2000;
 
-mysqli_query($link, "SET NAMES 'utf8'");
+// Вызовем наш метод:
+echo $user1->getAge(); // выведет '!!!'
 ?>
-
-<table border="1" cellspacing="1" cellpadding="1">
-    <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>age</th>
-        <th>salary</th>
-        <th>delete</th>
-    </tr>
-    <?php
-    // Сохранение нового (до получения!):
-    if (!empty($_POST)) {
-        $name = $_POST['name'];
-        $age = $_POST['age'];
-        $salary = $_POST['salary'];
-
-        $query = "INSERT INTO workers SET name='$name', age='$age', salary='$salary'";
-        mysqli_query($link, $query) or die(mysqli_error($link));
-    }
-
-    // Удаление по id (до получения!):
-    if (isset($_GET['del'])) {
-        $del = $_GET['del'];
-        $query = "DELETE FROM workers WHERE id=$del";
-        mysqli_query($link, $query) or die(mysqli_error($link));
-    }
-
-    // Получение всех работников:
-    $query = "SELECT * FROM workers";
-    $result = mysqli_query($link, $query) or die(mysqli_error($link));
-    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-
-    // Вывод на экран:
-    $result = '';
-    foreach ($data as $elem) {
-        $result .= '<tr>';
-
-        $result .= '<td>' . $elem['id'] . '</td>';
-        $result .= '<td>' . $elem['name'] . '</td>';
-        $result .= '<td>' . $elem['age'] . '</td>';
-        $result .= '<td>' . $elem['salary'] . '</td>';
-        $result .= '<td>' . $elem['salary'] . '</td>';
-        $result .= '<td><a href="?del=' . $elem['id'] . '">удалить</a></td>';
-
-        $result .= '</tr>';
-    }
-
-    echo $result;
-    ?>
-</table>
-<form action="" method="POST">
-    <input name="name">
-    <input name="age">
-    <input name="salary">
-    <input type="submit" value="добавить работника">
-</form>
