14e2307f004df87779eae57c7b37d05b64dc91b6
AlexVRBK <vvvorobok@gmail.com>
2021-06-15 14:49:47 +0300

    PHP.Трейты

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php
-class Employee
+trait Helper
 {
-    public $name;
-    public $age;
-    public $salary;
+    private $name;
+    private $age;
+    private $population;
 
-    // Создаем метод:
+    public function getName()
+    {
+        return $this->name;
+    }
+
     public function getAge()
     {
         return $this->age;
-        {
-            if ($this->getAge()>-18)
-                return 'true';
-            else 'false';
-        }
-        }
-
+    }
+    public function getPopulation()
+    {
+        return $this->population;
+    }
 }
+class Country
+{
+    use Helper;
 
-$user1 = new Employee;
-$user1->name = 'Коля';
-$user1->age = 12;
-$user1->salary = 1000;
-
-$user2 = new Employee;
-$user2->name = 'Вася';
-$user2->age = 16;
-$user2->salary = 2000;
-
-// Вызовем наш метод:
-echo $user1->getAge(); // выведет '!!!'
+    public function __construct($name, $age,$population)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->population = $population;
+    }
+}
+$country = new Country ('Ужгород', 102, 135000);
+echo $country->getName();
+echo '<br/>'.$country ->getAge();
+echo '<br/>'.$country->getPopulation();
 ?>
